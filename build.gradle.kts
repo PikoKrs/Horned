bdd11c7c9e2fda407f241c57a34af283ed067161
PikoKrs <dmitriimanulov@gmail.com>
2025-09-06 14:52:07 +0700

    Добавление зависимостей в систему сборки

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // implementation("org.springframework.boot:spring-boot-starter:3.5.5")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.5")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
