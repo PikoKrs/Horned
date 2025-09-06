b4baffc6fbd32387d7b68998f8c3e5c85105b662
PikoKrs <dmitriimanulov@gmail.com>
2025-09-06 15:02:28 +0700

    Добавление зависимостей в систему сборки

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
@@ -8,8 +10,16 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.2")
+    }
+}
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
